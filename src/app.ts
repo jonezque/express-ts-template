1dc9cc51c68b94db54a897f8b859798c300a5ab9
Artem Brilev <artem.brilev@skuvault.com>
2019-10-13 01:13:24 +0300

    werwe

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import usersRouter from './routes/users';
 import * as swaggerDocument from './swagger.json';
 
 passport.use(
-    new Strategy(function(token, done) {
+    new Strategy((token, done) => {
         console.log('token:' + token);
         const user = { name: 'hello' };
         done(null, user);
@@ -20,17 +20,14 @@ passport.use(
 );
 
 const app = express();
-console.log(config);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', passport.authenticate('bearer', { session: false }), usersRouter);
 app.use('/dummy', passport.authenticate('bearer', { session: false }), dummyRouter);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(config.port, function() {
-    console.log('Example app listening on port 3000!');
-});
+
+app.listen(config.port, () => console.log('Example app listening on port 3000!'));
